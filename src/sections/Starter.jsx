2652d4ec949228807632c203b1ff276c9c884742
Anton <anton.vosc@gmail.com>
2024-07-30 23:26:07 +0200

    Working with buttons

--- src/sections/Starter.jsx
+++ src/sections/Starter.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import WorkImage from "../assets/work.svg";
+import Button from "../components/Button";
+import {GoPlay} from "react-icons/go";
+import {HiOutlineArrowNarrowRight} from "react-icons/hi";
+import "../styles/sections/Starter.scss";
 
 function Starter() {
   return (
     <div className="main-container">
       <div className="container">
-        <h1>
-          This Website Has Been Created to Practice React Skills.
-        </h1>
+        <div className="content">
+          <h1>
+            This Website Has Been Created to Practice React Skills.
+          </h1>
 
-        <p>
-          This website has been created to demonstrate and develop React and Framer Motion skills.
-        </p>
+          <p>
+            This website has been created to demonstrate and develop React and Framer Motion skills.
+          </p>
 
-        <div className="button-container"></div>
-      </div>
+          <div className="button-container">
+              <Button content="Watch Video" icon={<GoPlay />} />
+              <Button content="Request Quote" icon={<HiOutlineArrowNarrowRight />} color="pink" />
+          </div>
+        </div>
 
-      <div className="image">
-        <img src={WorkImage} alt="Work Image" />
+        <div className="image">
+          <img src={WorkImage} alt="Work Image" />
+        </div>
       </div>
     </div>
   )
